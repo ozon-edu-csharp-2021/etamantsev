efd8ec2ef47180572cc91a1d81040712334094bc
Evgeny TamantsevEvgeny TamantsevEvgeny TamantsevEvgeny TamantsevEvgeny TamantsevEvgeny Tamantsev <bcrichwarlok230@gmail.com>
2021-10-22 11:37:43 +0300

    [#HOMEWORK-1] Правки по ревью.

--- src/OzonEdu.MerchandiseService/Startup.cs
+++ src/OzonEdu.MerchandiseService/Startup.cs
@@ -1,10 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace OzonEdu.MerchandiseService
 {
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-
     public class Startup
     {
         public Startup(IConfiguration configuration)
